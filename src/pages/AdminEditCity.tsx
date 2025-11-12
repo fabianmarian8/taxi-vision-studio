90a1d87a0a5213496f2a18aa2db3ca1f8ce2d0ef
fabianmarian8 <fabianmarian8@gmail.com>
2025-11-12 18:27:11 +0000

    chore: replace placeholder with minimal compiling stub to unblock build

--- src/pages/AdminEditCity.tsx
+++ src/pages/AdminEditCity.tsx
@@ -1,1 +1,4 @@
-<REPLACE_WITH_UPDATED_CONTENT>
+import React from 'react';
+export default function AdminEditCity() {
+  return (<div style={{padding:20}}><h1>Admin Edit City</h1><p>Dočasná verzia stránky. Prebieha refaktor. Funkčná verzia bude doplnená v ďalšom commite.</p></div>);
+}
